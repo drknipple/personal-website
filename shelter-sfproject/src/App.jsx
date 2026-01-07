935ca3b16d2ff7fc9d1b57837b479455a6c42599
drknipple <2893276+drknipple@users.noreply.github.com>
2026-01-07 07:41:02 -0500

    Added phone and details to LocationCard.

--- shelter-sfproject/src/App.jsx
+++ shelter-sfproject/src/App.jsx
@@ -2,10 +2,34 @@ import LocationCard from './components/LocationCard';
 
 function App() {
   const places = [
-    { name: "Franciscan Kitchen", type: "feeding program", address: "748 S Preston St"},
-    { name: "Arthr Street Hotel", type: "shelter", address: "1620 Arthur St"},
-    { name: "Goodwill Recource Center", type: "services", address: "909 E Broadwat"},
-    { name: "House of Ruth", type: "other temporary/permanent housing", address: "607 E St Catherine St"}
+    { 
+      name: "Franciscan Kitchen", 
+      type: "feeding program", 
+      address: "748 S Preston St",
+      phone: "(415) 555-1234",
+      details: "A feeding program for the homeless"
+    },
+    { 
+      name: "Arthr Street Hotel", 
+      type: "shelter", 
+      address: "1620 Arthur St",
+      phone: "(415) 555-1234",
+      details: "A shelter for the homeless"
+    },
+    { 
+      name: "Goodwill Recource Center", 
+      type: "services", 
+      address: "909 E Broadwat",
+      phone: "(415) 555-1234",
+      details: "A resource center for the homeless"
+    },
+    { 
+      name: "House of Ruth", 
+      type: "other temporary/permanent housing", 
+      address: "607 E St Catherine St",
+      phone: "(415) 555-1234",
+      details: "A temporary/permanent housing for the homeless"
+    }
   ];
 
   return (
@@ -17,6 +41,8 @@ function App() {
           name={place.name}
           type={place.type}
           address={place.address}
+          phone={place.phone}
+          details={place.details}
         />
       ))}
     </div>
